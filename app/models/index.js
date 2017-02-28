b8d1abac7849c442ff7cb85b864e202191bdd434
fix20152@gmail.com <fix20152@gmail.com>
2017-02-28 10:52:04 +0200

    commit for heroku

--- app/models/index.js
+++ app/models/index.js
@@ -3,7 +3,8 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "development";
+// var env       = process.env.NODE_ENV || "development";
+var env       = "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 
 if (process.env.DATABASE_URL) {
